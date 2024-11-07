3660b83d74ec75e4019185ed1359441bc95ce83f
eloicalderonsalvans <164076338+eloicalderonsalvans@users.noreply.github.com>
2024-11-07 16:53:00 +0100

    Agregar archivos de proyecto.

---
+++ Projecta_Kanban/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Projecta_Kanban
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
